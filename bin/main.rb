e537062ee51d88b5d2de56e0139206fa6b325cdc
Alexis <psialedev@gmail.com>
2020-03-08 18:01:39 -0500

    Working on debbuging a xpath

--- bin/main.rb
+++ bin/main.rb
@@ -2,23 +2,11 @@ require 'open-uri'
 require 'nokogiri'
 require_relative '../lib/classes/nokogiri_object.rb'
 
-short_quote = NokogiriObject.new("https://anyiko.blog/2011/10/06/12-qoutes-from-%E2%80%98100-years-of-solitude%E2%80%99/")
+# Frases en inglés 
+short_quote = NokogiriObject.new("https://www.goodreads.com/work/quotes/3295655-cien-a-os-de-soledad")
 
-sqp = short_quote.nokogiri_builder(short_quote.url)
-sqp.css("div#jp-post-flair").remove
-p sqp.css("p").text
-
-#  eu_url = open("https://www.europapress.es/cultura/libros-00132/noticia-cien-anos-soledad-cumple-50-anos-15-frases-imborrables-obra-gabriel-garcia-marquez-20170604115514.html").read
-#  eu_path = "//div[@class='NormalTextoNoticia']/p"
-#  europress = Nokogiri::HTML(eu_url)
-#  arr_euro = []
-#  euro_noko = europress.xpath(eu_path)
-#
-# euro_noko.each{ |quote|  arr_euro << quote.text}
-#
-# final_1 = arr_euro[5..-1]
-
-# Frases en inglés
+sqp = short_quote.nokogiri_builder(short_quote.url,"//div[@class = 'quote']/*").text
+p sqp
 
 
 # Frases en español
