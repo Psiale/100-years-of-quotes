1fcaae38d2df08f87b2aae85686b8b22e5795b2f
Alexis <psialedev@gmail.com>
2020-03-08 15:16:55 -0500

    created the basic structure of the project and added some classes and a module

--- lib/classes/nokogiri_object.rb
+++ lib/classes/nokogiri_object.rb
@@ -2,43 +2,29 @@ require 'nokogiri'
 require 'open-uri'
 require_relative '../modules/helperable.rb'
 class NokogiriObject
-    include Helperable
-        attr_accessor(:url)
-        def initialize(url)
-            @url = url
-        end
-def nokogiri_builder(url,paths = "")
+  include Helperable
+  attr_accessor(:url)
+  def initialize(url)
+    @url = url
+  end
+
+  def nokogiri_builder(url, paths = "")
     url_parsed = URI.parse(url)
     nokogiri = Nokogiri::HTML("#{open(url_parsed).read}")
     nokogiri_path = path_validator(nokogiri, paths)
-end
+  end
 
-def path_validator(nokogiri_object, paths)
+  def path_validator(nokogiri_object, paths)
     if !paths.empty?
-        case paths
-        when paths.include?("/")
-         nokogiri_object.xpath(paths)
-        else
-            nokogiri_object.css(paths)
-        end
-        
-    else
-        nokogiri_object
-        end
-    end
-end
+      case paths
+      when paths.include?("/")
+        nokogiri_object.xpath(paths)
+      else
+        nokogiri_object.css(paths)
+      end
 
-#avatar = Nokogiri_object.new("https://avatarteam.es.tl/FRASES-CELEBRES-DE-LA-SERIE.htm").nokogiri_builder(,"//p")
-#arr_avatar = [] 
-#
-# avatar.split(" ").each{ |element| arr_avatar << element}
-## avatar.each{ |quote| quote # arr_avatar << quote.text.split(" ")}
-#
-# 8.times do |i|
-#    i = 0
-#    # puts arr_avatar[i]
-#    arr_avatar.delete_at(i)
-#    # puts arr_avatar
-# end
-#
-#arr_avatar.join(" ")
+    else
+      nokogiri_object
+        end
+      end
+end
--- lib/modules/helperable.rb
+++ lib/modules/helperable.rb
@@ -1,15 +1,15 @@
 module Helperable
-    def path_validator(nokogiri_object, paths)
-        if !paths.empty?
-            case paths
-            when paths.include?("/")
-             nokogiri_object.xpath(paths).text
-            else
-                nokogiri_object.css(paths).text
-            end
-            
-        else
-            puts noo!
-        end
+  def path_validator(nokogiri_object, paths)
+    if !paths.empty?
+      case paths
+      when paths.include?('/')
+        nokogiri_object.xpath(paths).text
+      else
+        nokogiri_object.css(paths).text
+      end
+
+    else
+      puts noo!
     end
+  end
 end
