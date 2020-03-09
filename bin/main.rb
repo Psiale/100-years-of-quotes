e6057abc550e14e99ed3b59b1a8bb8e62c1ea2da
Alexis <psialedev@gmail.com>
2020-03-08 21:10:53 -0500

    finally added some strings with good format

--- bin/main.rb
+++ bin/main.rb
@@ -3,12 +3,23 @@ require 'nokogiri'
 require_relative '../lib/classes/nokogiri_object.rb'
 
 # Frases en inglés 
-short_quote = NokogiriObject.new("https://www.goodreads.com/work/quotes/3295655-cien-a-os-de-soledad")
+short_quote = NokogiriObject.new("https://www.litcharts.com/lit/one-hundred-years-of-solitude/quotes")
 
-sqp = short_quote.nokogiri_builder(short_quote.url,"//div[@class = 'quote']/*").text
-p sqp
+sqp = short_quote.nokogiri_builder(short_quote.url,"//p[@class='quote-text']")
+arr_of_short_quotes = []
+hash_of_quotes = { }
 
+ sqp.each do |quote|
+    arr_of_short_quotes << quote.text.gsub(/ \Wu201\w([\W])* /," ")
+ end
 
+ arr_of_short_quotes.each_with_index { |item, index|
+    hash_of_quotes[index] = item  }
+
+ puts hash_of_quotes.values
+
+ # so far I have the quotes now I have to join some of them
+ # I fucking hate this shit
 # Frases en español
 # life_url = open("https://www.lifeder.com/frases-de-cien-anos-de-soledad/").read
 # life_path = "//p/span"
